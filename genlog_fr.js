dab59a27532e96208caff0434fadf01d641f04fa
tcrouzet <tc@tcrouzet.com>
2024-05-20 20:21:32 +0200

    genlogNewgen

--- genlog_fr.js
+++ genlog_fr.js
@@ -1,1 +1,1 @@
-function loadContent(callback) { var link = { "url": "https://genlog.tcrouzet.com/2024/05/16_fr/", "title": "Jeudi 16 mai 2024" }; callback(link); }
+function loadContent(callback) { var link = { "url": "https://genlog.tcrouzet.com/2024/05/20_fr/", "title": "Lundi 20 mai 2024" }; callback(link); }
